9a15fa8324fdc9561fe40c37b96f0b580b6292c5
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-06-14 12:29:38 +0800

    feat: added SaveContext (#9)
    * adding all the commands about department and save/load
    
    * updated
    
    * With SaveContext

--- src/lib.rs
+++ src/lib.rs
@@ -14,29 +14,11 @@ pub mod traits;
 
 use crate::{
 	context::Context,
-	menu_items::{CreateDepartment, ListDepartments, MenuItem, NameCompany, PrintContext, ShowDepartment},
+	menu_items::{
+		CreateDepartment, ListDepartments, LoadContext, MenuItem, NameCompany, PrintContext, Quit,
+		SaveContext, ShowDepartment,
+	},
 };
-
-fn display_menu(menu_items: &[Box<dyn MenuItem>]) -> Result<(), Box<dyn error::Error>> {
-	println!("What do you want to do?");
-
-	for (idx, item) in menu_items.iter().enumerate() {
-		println!(
-			"{}. {:40}{}",
-			idx + 1,
-			item.menuitem_txt(),
-			match item.hotkey() {
-				Some(ks) => format!("[{ks}]"),
-				None => "".to_string(),
-			}
-		);
-	}
-
-	print!("? ");
-	let _ = io::stdout().flush();
-
-	Ok(())
-}
 
 pub fn run() -> Result<(), Box<dyn error::Error>> {
 	let mut ctx = Context::new();
@@ -45,9 +27,12 @@ pub fn run() -> Result<(), Box<dyn error::Error>> {
 		Box::new(NameCompany::new()),
 		Box::new(ListDepartments::new()),
 		Box::new(CreateDepartment::new()),
-		Box::new(ShowDepartment::new()),
+		Box::new(ShowDepartment()),
+		Box::new(SaveContext()),
+		Box::new(LoadContext()),
 		#[cfg(debug_assertions)]
-		Box::new(PrintContext::new()),
+		Box::new(PrintContext()),
+		Box::new(Quit()),
 	];
 	let re_digits = Regex::new(r"\d+$")?;
 
@@ -77,10 +62,51 @@ pub fn run() -> Result<(), Box<dyn error::Error>> {
 				// Append one more newline at the end
 				println!();
 			}
-			"q" => break,
+			t if !t.is_empty() => {
+				if let Some(mi) = get_menu_item_from_shortcut(&menu_items, t) {
+					let _ = mi.execute(&mut ctx);
+				} else {
+					println!("Invalid shortcut");
+				}
+			}
 			_ => continue,
 		};
 	}
+}
+
+/**
+ * Un-export / internal helper methods below
+ **/
+fn display_menu(menu_items: &[Box<dyn MenuItem>]) -> Result<(), Box<dyn error::Error>> {
+	println!("What do you want to do?");
+
+	for (idx, item) in menu_items.iter().enumerate() {
+		println!(
+			"{}. {:40}{}",
+			idx + 1,
+			item.menuitem_txt(),
+			if let Some(ks) = item.shortcut() { format!("[{ks}]") } else { "".to_string() }
+		);
+	}
+
+	print!("? ");
+	let _ = io::stdout().flush();
 
 	Ok(())
+}
+
+fn get_menu_item_from_shortcut<'a>(
+	menu_items: &'a Vec<Box<dyn MenuItem>>,
+	shortcut: &'a str,
+) -> Option<&'a dyn MenuItem> {
+	if shortcut.is_empty() {
+		return None;
+	}
+
+	menu_items
+		.iter()
+		.filter(|mi| mi.shortcut().map_or("", |sc| sc) == shortcut)
+		.collect::<Vec<_>>()
+		.first()
+		.map(|bv| &***bv)
 }
