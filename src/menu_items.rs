7490bdf88d474e4601579cd59a52cff32f1bcec6
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-06-19 10:59:44 +0800

    updated

--- src/menu_items.rs
+++ src/menu_items.rs
@@ -2,7 +2,7 @@ use std::{error, fs, io, path::Path};
 
 use crate::{
 	context::Context,
-	department::{Department, DepartmentId},
+	department::{Department, DepartmentId, DepartmentInfo},
 	errors::ApplicationError,
 	traits::OneLiner,
 };
@@ -11,24 +11,24 @@ pub trait MenuItem {
 	fn menuitem_txt(&self) -> &str;
 	fn shortcut(&self) -> Option<&str>;
 	fn execute_interactive(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>>;
-	fn execute(
+	fn execute<'a>(
 		&self,
-		ctx: &mut Context,
+		ctx: &'a mut Context,
 		input: MenuItemInput,
-	) -> Result<MenuItemOutput, Box<dyn error::Error>>;
+	) -> Result<MenuItemOutput<'a>, Box<dyn error::Error>>;
 }
 
 #[derive(Debug)]
 pub enum MenuItemInput {
 	String(String),
-	DepartmentInfo(String, Option<DepartmentId>),
+	DepartmentParams(String, Option<String>),
 	None,
 }
 
 #[derive(Debug)]
-pub enum MenuItemOutput {
+pub enum MenuItemOutput<'a> {
 	String(String),
-	Department(Department),
+	DepartmentInfo(DepartmentInfo<'a>),
 	None,
 }
 
@@ -65,13 +65,13 @@ impl MenuItem for NameCompany {
 		Ok(())
 	}
 
-	fn execute(
+	fn execute<'a>(
 		&self,
-		ctx: &mut Context,
+		ctx: &'a mut Context,
 		input: MenuItemInput,
-	) -> Result<MenuItemOutput, Box<dyn error::Error>> {
+	) -> Result<MenuItemOutput<'a>, Box<dyn error::Error>> {
 		let MenuItemInput::String(input) = input else {
-			return Err(Box::new(ApplicationError(format!("Unrecognized input: {:?}", input))));
+			Err(Box::new(ApplicationError(format!("Unrecognized input: {:?}", input))))?
 		};
 
 		ctx.set_company_name(input.to_string());
@@ -119,18 +119,18 @@ impl MenuItem for ListDepartments {
 
 	fn execute_interactive(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>> {
 		let MenuItemOutput::String(result) = self.execute(ctx, MenuItemInput::None)? else {
-			return Err(Box::new(ApplicationError("Unrecognized output".to_string())));
+			Err(Box::new(ApplicationError("Unrecognized output".to_string())))?
 		};
 
-		println!("{}", result);
+		println!("{result}");
 		Ok(())
 	}
 
-	fn execute(
+	fn execute<'a>(
 		&self,
-		ctx: &mut Context,
+		ctx: &'a mut Context,
 		_input: MenuItemInput,
-	) -> Result<MenuItemOutput, Box<dyn error::Error>> {
+	) -> Result<MenuItemOutput<'a>, Box<dyn error::Error>> {
 		let mut result = format!("{}\n", ctx.company_name());
 
 		let dep_str = ctx
@@ -169,7 +169,7 @@ impl MenuItem for CreateDepartment {
 		Some(&self.shortcut)
 	}
 
-	fn execute(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>> {
+	fn execute_interactive(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>> {
 		println!("What's the name of the new department?");
 		let mut name = String::new();
 		io::stdin().read_line(&mut name)?;
@@ -178,23 +178,40 @@ impl MenuItem for CreateDepartment {
 		println!(
 			"Does this department has a parent department?\n(Press \"Enter\" for none, or enter the department ID)"
 		);
-
 		let mut parent_dep = String::new();
 		io::stdin().read_line(&mut parent_dep)?;
+		let parent_dep = parent_dep.trim();
+		let parent_dep = if parent_dep.len() > 0 { Some(parent_dep.to_string()) } else { None };
 
-		let maybe_parent_dep_id = if parent_dep.trim() == "" {
-			None
-		} else {
-			match DepartmentId::try_from(parent_dep.trim())? {
-				dep_id if ctx.validate_department_id(&dep_id) => Some(dep_id),
-				_ => return Err(Box::new(ApplicationError("Unknown department".to_string()))),
-			}
+		self.execute(ctx, MenuItemInput::DepartmentParams(name.to_string(), parent_dep)).map(|_| ())
+	}
+
+	fn execute<'a>(
+		&self,
+		ctx: &'a mut Context,
+		input: MenuItemInput,
+	) -> Result<MenuItemOutput<'a>, Box<dyn error::Error>> {
+		let MenuItemInput::DepartmentParams(name, parent_dep_id) = input else {
+			Err(Box::new(ApplicationError("Unrecognized input".to_string())))?
 		};
 
-		let new_department = Department::new(ctx.get_next_department_id(), name, maybe_parent_dep_id);
+		let parent_dep_id = match parent_dep_id {
+			Some(parent_dep_id) => {
+				let parent_dep_id = <DepartmentId as TryFrom<&str>>::try_from(&parent_dep_id)?;
+
+				if ctx.validate_department_id(&parent_dep_id) {
+					Some(parent_dep_id)
+				} else {
+					Err(Box::new(ApplicationError("Unknown department".to_string())))?
+				}
+			}
+			None => None,
+		};
+
+		let new_department = Department::new(ctx.get_next_department_id(), &name, parent_dep_id);
 		ctx.insert_department(new_department);
 
-		Ok(())
+		Ok(MenuItemOutput::None)
 	}
 }
 
@@ -212,20 +229,37 @@ impl MenuItem for ShowDepartment {
 		Some("rd")
 	}
 
-	fn execute(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>> {
+	fn execute_interactive(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>> {
 		let max_input = Into::<u32>::into(*ctx.next_department_id()) - 1;
 		println!("Which department do you want to show? (0 - {})", max_input);
 
 		let mut input = String::new();
 		io::stdin().read_line(&mut input)?;
 
-		if let Some(dep_info) = ctx.department_info(&DepartmentId(input.trim().parse::<u32>()?)) {
-			println!("{}", dep_info);
-		} else {
-			return Err(Box::new(ApplicationError("Unknown department".to_string())));
-		}
+		let MenuItemOutput::DepartmentInfo(dep_info) =
+			self.execute(ctx, MenuItemInput::String(input.trim().to_string()))?
+		else {
+			Err(Box::new(ApplicationError("Unrecognized output".to_string())))?
+		};
 
+		println!("{dep_info}");
 		Ok(())
+	}
+
+	fn execute<'a>(
+		&self,
+		ctx: &'a mut Context,
+		input: MenuItemInput,
+	) -> Result<MenuItemOutput<'a>, Box<dyn error::Error>> {
+		let MenuItemInput::String(dep_id) = input else {
+			Err(Box::new(ApplicationError("Unrecognized input".to_string())))?
+		};
+
+		let Some(dep_info) = ctx.department_info(&DepartmentId(dep_id.parse::<u32>()?)) else {
+			Err(Box::new(ApplicationError("Invalid department ID".to_string())))?
+		};
+
+		Ok(MenuItemOutput::DepartmentInfo(dep_info))
 	}
 }
 
@@ -243,9 +277,21 @@ impl MenuItem for PrintContext {
 		None
 	}
 
-	fn execute(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>> {
-		println!("{:#?}", ctx);
+	fn execute_interactive(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>> {
+		let MenuItemOutput::String(output) = self.execute(ctx, MenuItemInput::None)? else {
+			Err(Box::new(ApplicationError("Unrecognized output".to_string())))?
+		};
+
+		println!("{output}");
 		Ok(())
+	}
+
+	fn execute<'a>(
+		&self,
+		ctx: &'a mut Context,
+		_input: MenuItemInput,
+	) -> Result<MenuItemOutput<'a>, Box<dyn error::Error>> {
+		Ok(MenuItemOutput::String(format!("{:#?}", ctx)))
 	}
 }
 
@@ -263,15 +309,26 @@ impl MenuItem for SaveContext {
 		Some("s")
 	}
 
-	fn execute(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>> {
+	fn execute_interactive(&self, ctx: &mut Context) -> Result<(), Box<dyn error::Error>> {
 		println!("Which file path to save to?");
 		let mut filepath = String::new();
 		io::stdin().read_line(&mut filepath)?;
-		let filepath = filepath.trim();
+
+		self.execute(ctx, MenuItemInput::String(filepath.trim().to_string())).map(|_| ())
+	}
+
+	fn execute<'a>(
+		&self,
+		ctx: &'a mut Context,
+		input: MenuItemInput,
+	) -> Result<MenuItemOutput<'a>, Box<dyn error::Error>> {
+		let MenuItemInput::String(filepath) = input else {
+			Err(Box::new(ApplicationError(format!("Unrecognized input: {:?}", input))))?
+		};
 		let path = Path::new(&filepath);
 
 		let serialized = serde_json::to_string(ctx)?;
-		fs::write(path, serialized).map_err(|e| e.into())
+		fs::write(path, serialized).map(|_| MenuItemOutput::None).map_err(|e| e.into())
 	}
 }
 
@@ -298,13 +355,13 @@ impl MenuItem for LoadContext {
 		self.execute(ctx, MenuItemInput::String(filepath.trim().to_string())).map(|_| ())
 	}
 
-	fn execute(
+	fn execute<'a>(
 		&self,
-		ctx: &mut Context,
+		ctx: &'a mut Context,
 		input: MenuItemInput,
-	) -> Result<MenuItemOutput, Box<dyn error::Error>> {
+	) -> Result<MenuItemOutput<'a>, Box<dyn error::Error>> {
 		let MenuItemInput::String(filepath) = input else {
-			return Err(Box::new(ApplicationError(format!("Unrecognized input: {:?}", input))));
+			Err(Box::new(ApplicationError(format!("Unrecognized input: {:?}", input))))?
 		};
 
 		let data_filepath = Path::new(&filepath);
@@ -334,7 +391,11 @@ impl MenuItem for Quit {
 		self.execute(ctx, MenuItemInput::None).map(|_| ())
 	}
 
-	fn execute(&self, _: &mut Context, _: MenuItemInput) -> Result<MenuItemOutput, Box<dyn error::Error>> {
+	fn execute<'a>(
+		&self,
+		_: &'a mut Context,
+		_: MenuItemInput,
+	) -> Result<MenuItemOutput<'a>, Box<dyn error::Error>> {
 		std::process::exit(0);
 	}
 }
